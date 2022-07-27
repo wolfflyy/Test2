d9cee0961a1fe3da881c42190cd8a1790869cba5
wolfflyy <90817807+wolfflyy@users.noreply.github.com>
2022-07-27 14:44:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<form class="box">
+	<h1>LOGIN</h1>
+	<input type="text" name="username" placeholder="Username">
+	<input type="password" name="password" placeholder="Password">
+	<input type="submit" value="Login">
+	</form>
+</body>
+</html>
