0ba90a57a871f75502c76f8b50e5766989bd4c36
wolfflyy <90817807+wolfflyy@users.noreply.github.com>
2022-07-27 14:46:55 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,29 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<form class="box">
-	<h1>LOGIN</h1>
-	<input type="text" name="username" placeholder="Username">
-	<input type="password" name="password" placeholder="Password">
-	<input type="submit" value="Login">
-	</form>
+
+	<div class="container">
+		<div class="navbar">
+		<div class="logo">
+			<img src="images/atn-tv-logo-C934D4C2E5-seeklogo.com.png" width="125px">
+		</div>
+		<nav>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Products</a></li>
+				<li><a href="">About</a></li>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Account</a></li>		
+			</ul>
+		</nav>
+	</div>
+	<div class="row">
+		<div class="col-2">
+			<h1>Go Buy Your Kids<br>New Toys!<</h1>
+		</div>
+		<div class="col-2">
+			<img src="images/shutterstock_383521510-002-1024x585.jpg">
+		</div>
+	</div>	
 </body>
 </html>
